f3d442034f3a7e5af8941ec691e54f49c62469ce
neMisha1234 <micha_kravtsov_olymp@mail.ru>
2025-11-25 18:16:08 +0300

    Добавил просмотр анкет и начал делать кнопку "Лайки"

--- APIwork/test.py
+++ APIwork/test.py
@@ -1,4 +1,8 @@
 import requests
-
-response = requests.get("https://kudago.com/public-api/v1.4/event-categories/")
-print(response.json())
+from datetime import datetime
+def get_info_event():
+    url = 'https://kudago.com/public-api/v1.2/events/?page_size=1&expand=place&location=msk&dates,participants&fields=dates,id,title,description,place,location'
+    event = requests.get(url).json()
+    txt = 'Событие:'+event['results'][0]['title']+'\n'+'Описание:'+event['results'][0]['description'].strip('<p>').replace('</p>','')+'Дата:'+ str(datetime.fromtimestamp(event['results'][0]['dates'][0]['start']).strftime('%Y-%m-%d'))
+    return txt
+print(get_info_event())
--- db_tools.py
+++ db_tools.py
@@ -31,6 +31,23 @@ def load_to_db(tg_id, name, age, city, inst, genres, description, exp):
     db_sess.commit()
 
 
+def delete_user(tg_id):
+    user = db_sess.query(User).filter(User.telegram_id == tg_id).first()
+    db_sess.delete(user)
+
+
+def get_user(tg_id):
+    return db_sess.query(User).filter(User.telegram_id == tg_id).first()
+
+
+def get_liked_users(uid):
+    lst = []
+    for user in db_sess.query(User).all():
+        if (';' + str(uid) + ';') in user.favorite_users:
+            lst.append(user)
+    return lst
+
+
 def save_user(user):
     db_sess.add(user)
     db_sess.commit()
@@ -101,4 +118,15 @@ def build_anket(tg_id):
 
 
 def search_users(city='', age='', insts='', genres=''):
-    pass
+    users = db_sess.query(User).all()
+    if city:
+        users = list(filter(lambda x: x.city == city.capitalize(), users))
+    if age:
+        users = list(filter(lambda x: age[0] <= x.age <= age[1], users))
+    if insts:
+        for inst in insts.split(';'):
+            users = list(filter(lambda x: inst in x.inst, users))
+    if genres:
+        for gen in genres.split(';'):
+            users = list(filter(lambda x: gen in x.gens, users))
+    return users
